50929861f493358b1809b8b29dd11eff77a4ddfc
bill <bill.xing@nexttao.com>
2021-12-04 02:44:00 +0800

    tekton

--- generate.rb
+++ generate.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 f = ARGV[0]
 raise 'empty image' if f.nil? || f.empty?
-path = f.gsub(":", "/")
+path = f.gsub(":", "/").gsub("@", "/")
 exit 1 if !system("mkdir -p #{path}")
 File.open(path + "/Dockerfile", "w") do |df|
   df.write("FROM #{f}")
 end
 puts "#{path}/"
-puts f.gsub("/", "_").gsub(":","-")
+puts f.gsub("/", "_").gsub(":","-").gsub("@", "-")
